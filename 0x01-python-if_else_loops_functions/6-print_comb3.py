52ac2547a530bbe153d6627539bd2246550e9eb4
AmonMunyai <Amonmunyai11@gmail.com>
2023-01-04 15:24:07 +0200

    fixed duplicate 89

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(1, 10):
-        if not (i >= j):
+        if (not (i >= j)) and i < 8:
             print("{0:d}{1:d}".format(i, j), end=", ")
 print(89)
