148b1963217e2a5f49ece77aceb6d43a0ee5639b
AmonMunyai <Amonmunyai11@gmail.com>
2023-01-12 01:25:32 +0200

    added return 0

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
 def weight_average(my_list=[]):
+    if not isinstance(my_list, list) or len(my_list) == 0:
+        return (0)
+
     w, res = 0, 0
 
     for i, j in my_list:
