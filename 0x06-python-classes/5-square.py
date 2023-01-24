7cfcc3270a1dc6382c8e9bed91cec990a8841d3e
AmonMunyai <Amonmunyai11@gmail.com>
2023-01-25 00:18:50 +0200

    Update Documentation

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,11 +1,19 @@
 #!/usr/bin/python3
+"""Define a class Square."""
 
 class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
 
     @property
     def size(self):
+        """Retrieve the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -17,9 +25,11 @@ class Square:
         self.__size = value
 
     def area(self):
+        """Return the current square area."""
         return (self.__size * self.__size)
 
     def my_print(self):
+        """Prints square in stdout using the '#' character."""
         if (self.__size == 0):
             print("")
             return
